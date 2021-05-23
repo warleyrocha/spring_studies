9d70c029edce40cca18b47e46fd9d3d5ed440df3
Warley Rocha <P1190199@brasilprev.com.br>
2021-05-23 18:20:26 -0300

    Project hr-payroll FeignClient

---
+++ hr-payroll/src/main/java/br/com/devquick/hrpayroll/feing/WorkerFeingClient.java
@@ -0,0 +1,16 @@
+package br.com.devquick.hrpayroll.feing;
+
+import br.com.devquick.hrpayroll.entities.Worker;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/worker")
+public interface WorkerFeingClient {
+
+    @GetMapping("{id}")
+    ResponseEntity<Worker> findById(@PathVariable Long id);
+}
--- hr-payroll/src/main/java/br/com/devquick/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/br/com/devquick/hrpayroll/service/PaymentService.java
@@ -2,31 +2,27 @@ package br.com.devquick.hrpayroll.service;
 
 import br.com.devquick.hrpayroll.entities.Payment;
 import br.com.devquick.hrpayroll.entities.Worker;
+import br.com.devquick.hrpayroll.feing.WorkerFeingClient;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class PaymentService {
 
-    public final RestTemplate restTemplate;
-
-    @Value(value = "${hr-worker.host}")
-    private String workerHost;
+    public final WorkerFeingClient workerFeingClient;
 
     public Payment getPayment(Long workerId, int days) {
-        Map<String, String> uriVariables = new HashMap();
-        uriVariables.put("id", workerId.toString());
 
-        Worker worker = restTemplate.getForObject(workerHost + "/{id}", Worker.class, uriVariables);
-        return Payment.builder()
-                .name(worker.getName())
-                .days(days)
-                .dailyIncome(worker.getDailyIncome()).build();
+        ResponseEntity<Worker> responseWorker = workerFeingClient.findById(workerId);
+        if (responseWorker.getStatusCode().is2xxSuccessful()) {
+            Worker worker = responseWorker.getBody();
+            return Payment.builder()
+                    .name(worker.getName())
+                    .days(days)
+                    .dailyIncome(worker.getDailyIncome()).build();
+        }
+        return null;
     }
 }
