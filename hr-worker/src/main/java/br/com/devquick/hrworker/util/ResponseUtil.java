c341d36b0d0402b506225246a25a1eea2b601925
Warley Rocha <P1190199@brasilprev.com.br>
2021-05-23 16:21:44 -0300

    Project hr-payroll created

--- hr-worker/src/main/java/br/com/devquick/hrworker/util/ResponseUtil.java
+++ hr-worker/src/main/java/br/com/devquick/hrworker/util/ResponseUtil.java
@@ -2,15 +2,12 @@ package br.com.devquick.hrworker.util;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.ResponseEntity.BodyBuilder;
 
 import java.util.Optional;
 
 public interface ResponseUtil {
 
-    static <X> ResponseEntity<X> wrapOrNotFound(Optional<X> maybeResponse) {
-        return (ResponseEntity) maybeResponse.map((response) -> {
-            return ((BodyBuilder) ResponseEntity.ok()).body(response);
-        }).orElse(new ResponseEntity(HttpStatus.NOT_FOUND));
+    static ResponseEntity wrapOrNotFound(Optional<?> maybeResponse) {
+        return maybeResponse.map((response) -> ResponseEntity.ok().body(response)).orElse(new ResponseEntity(HttpStatus.NOT_FOUND));
     }
 }
