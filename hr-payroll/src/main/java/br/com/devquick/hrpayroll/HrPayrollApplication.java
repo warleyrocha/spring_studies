858730bceaed9b2702ef13836d9f2e7d71ea32ad
Warley Rocha <P1190199@brasilprev.com.br>
2021-05-26 22:44:48 -0300

    Eureka Clients Configured

--- hr-payroll/src/main/java/br/com/devquick/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/br/com/devquick/hrpayroll/HrPayrollApplication.java
@@ -2,12 +2,13 @@ package br.com.devquick.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@RibbonClient("hr-worker")
+@EnableEurekaClient
 public class HrPayrollApplication {
 
 	public static void main(String[] args) {
--- hr-worker/src/main/java/br/com/devquick/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/br/com/devquick/hrworker/HrWorkerApplication.java
@@ -2,10 +2,12 @@ package br.com.devquick.hrworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableEurekaClient
 public class HrWorkerApplication {
 
 	public static void main(String[] args) {
