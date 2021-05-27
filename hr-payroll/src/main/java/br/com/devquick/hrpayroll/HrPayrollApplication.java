7b022eba92b8f8ad8c68cf4c16fcb35812dbcb25
Warley Rocha <P1190199@brasilprev.com.br>
2021-05-26 23:40:59 -0300

    Hystrics fault tolerance

--- hr-payroll/src/main/java/br/com/devquick/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/br/com/devquick/hrpayroll/HrPayrollApplication.java
@@ -2,6 +2,7 @@ package br.com.devquick.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -9,6 +10,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class HrPayrollApplication {
 
 	public static void main(String[] args) {
--- hr-payroll/src/main/java/br/com/devquick/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/br/com/devquick/hrpayroll/resources/PaymentResource.java
@@ -2,12 +2,15 @@ package br.com.devquick.hrpayroll.resources;
 
 import br.com.devquick.hrpayroll.entities.Payment;
 import br.com.devquick.hrpayroll.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("payment")
@@ -16,8 +19,15 @@ public class PaymentResource {
 
     private final PaymentService paymentService;
 
+    @HystrixCommand(fallbackMethod = "getpaymentAlternative")
     @GetMapping("{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
         return ResponseEntity.ok(paymentService.getPayment(workerId, days));
     }
+
+    public ResponseEntity<Payment> getpaymentAlternative(Long workerId, Integer days) {
+        return ResponseEntity.ok(
+                Payment.builder().name("Brann").days(days).dailyIncome(BigDecimal.valueOf(400)).build());
+    }
+
 }
