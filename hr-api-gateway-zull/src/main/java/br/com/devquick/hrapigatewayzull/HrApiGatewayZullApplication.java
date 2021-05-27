c086449157b5cbba01cb55d85601aa379e2a7d78
Warley Rocha <P1190199@brasilprev.com.br>
2021-05-26 23:46:08 -0300

    Api Gateway Zull Created

---
+++ hr-api-gateway-zull/src/main/java/br/com/devquick/hrapigatewayzull/HrApiGatewayZullApplication.java
@@ -0,0 +1,13 @@
+package br.com.devquick.hrapigatewayzull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiGatewayZullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGatewayZullApplication.class, args);
+	}
+
+}
